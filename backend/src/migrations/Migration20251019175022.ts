ecf8f4aaa595d5d26798a1bb8b40d883460c9012
Kim Jun Hyeong <ggprgrkjh@naver.com>
2025-10-20 02:53:01 +0900

    feat: 마이그레이션 스크립트 수정

--- backend/src/migrations/Migration20251019175022.ts
+++ backend/src/migrations/Migration20251019175022.ts
@@ -4,14 +4,10 @@ export class Migration20251019175022 extends Migration {
 
   override async up(): Promise<void> {
     this.addSql(`alter table "user" alter column "picture" type text using ("picture"::text);`);
-
-    this.addSql(`alter table "scan" add column "language" varchar(255) not null default 'ko';`);
   }
 
   override async down(): Promise<void> {
     this.addSql(`alter table "user" alter column "picture" type varchar(255) using ("picture"::varchar(255));`);
-
-    this.addSql(`alter table "scan" drop column "language";`);
   }
 
 }
